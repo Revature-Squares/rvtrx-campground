505808582e0bb2f299de61453d954b3a5fafe221
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-02-24 11:48:22 -0500

    trying to push to fills

--- angular/src/app/data/Mocks/rental.mock.ts
+++ angular/src/app/data/Mocks/rental.mock.ts
@@ -16,22 +16,7 @@ export const rentals: Rental[] = [
     lotNumber: '2',
     size: '5x5',
     capacity: 5,
-<<<<<<< HEAD
     siteName: 'rv',
-=======
-    SiteName: 'rv',
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> cbcce67... rebase issues fixed?
-=======
-=======
-      size: '5x5',
-      capacity: 5,
-      SiteName: 'rv',
->>>>>>> 1e95a88... fixed issues in testing with rental-unit
->>>>>>> 39df1a4... rebase stuff and things
-=======
->>>>>>> ad8c0f5... stage changes problem resolved
     status: 'available',
     price: 100,
   },
--- angular/src/app/data/rental.model.ts
+++ angular/src/app/data/rental.model.ts
@@ -15,39 +15,9 @@ export interface Rental {
   capacity: number;
   discountedPrice?: number;
   lotNumber: string;
-<<<<<<< HEAD
-=======
-  /** type of site
-   *  - plotSize
-   *    - an interface that is used to find out the size of a plot
-   *      ```yaml
-   *      width:number
-   *      height:number
-   *      ```
-   *  - amenities
-   *    - an interface that talks about what the campsite offers the camper
-   *      ```yaml
-   *      maxiumumCapacity:number
-   *      voltage:number
-   *      sewage:string
-   *      water:string
-   *      ```
-   */
-
-  /** booking status, one of:
-   *  - available (neither booked nor currently in use)
-   *  - booked (booked by someone else, but not in use)
-   *  - occupied (currently in use)
-   */
-  status: string;
-  /** normal nightly cost */
->>>>>>> 6454c38... fixed other compile issues with folding rental model
   price: number;
   siteName: string;
   size: string;
   status: string;
   lodgingRentalId?: number;
-  size: string;
-  capacity: number;
-  SiteName: string;
 }
